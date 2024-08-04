9a38b64ab6bca07d99080bf19bc31f5e5bc49794
FT49 <ft49@FT49s-iMac.local>
2024-08-04 12:24:57 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from 'react-dom/client';
+import App from "./App.jsx";
 import "./App.scss";
 
 const el = document.getElementById("app");
