4e7cc70bcf30d901f055e916d76b980c32c17abe
Jasen Tenney <jasen_tenney@hotmail.com>
2024-08-06 18:44:11 -0500

    Initial version of RegisterScreen

---
+++ frontend/src/components/iconify/iconify.jsx
@@ -0,0 +1,40 @@
+import { forwardRef } from 'react';
+import { Icon, disableCache } from '@iconify/react';
+
+import Box from '@mui/material/Box';
+import NoSsr from '@mui/material/NoSsr';
+
+import { iconifyClasses } from './classes';
+
+// ----------------------------------------------------------------------
+
+export const Iconify = forwardRef(({ className, width = 20, sx, ...other }, ref) => {
+  const baseStyles = {
+    width,
+    height: width,
+    flexShrink: 0,
+    display: 'inline-flex',
+  };
+
+  const renderFallback = (
+    <Box
+      component="span"
+      className={iconifyClasses.root.concat(className ? ` ${className}` : '')}
+      sx={{ ...baseStyles, ...sx }}
+    />
+  );
+
+  return (
+    <NoSsr fallback={renderFallback}>
+      <Box
+        ref={ref}
+        component={Icon}
+        className={iconifyClasses.root.concat(className ? ` ${className}` : '')}
+        sx={{ ...baseStyles, ...sx }}
+        {...other}
+      />
+    </NoSsr>
+  );
+});
+
+disableCache('local');
